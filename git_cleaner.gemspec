865d48ab49f5f3532edbbd96135299e71177b33e
KarimEbrahemAbdelaziz <karimabdelazizmansour@gmail.com>
2019-05-02 02:02:55 +0200

    Cleanup and Preview local and origin branches

--- git_cleaner.gemspec
+++ git_cleaner.gemspec
@@ -28,17 +28,19 @@ Gem::Specification.new do |spec|
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
-  spec.files = Dir.chdir(File.expand_path('..', __FILE__)) do
-    `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
-  end
+  # spec.files = Dir.chdir(File.expand_path('..', __FILE__)) do
+  #   `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  # end
+  spec.files = Dir.glob("{bin,lib}/**/*")
   spec.bindir        = "exe"
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.executables   = ["git_cleaner"]
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "thor"
-  spec.add_dependency "highline"
-  spec.add_dependency "rainbow"
-  spec.add_dependency "httpclient"
+  spec.add_dependency "thor", "~> 0.20.3"
+  spec.add_dependency "highline", "~> 2.0.2"
+  spec.add_dependency "rainbow", "~> 3.0.0"
+  spec.add_dependency "git", "~> 1.5.0"
+  spec.add_dependency "httpclient", "~> 2.8.3"
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
--- lib/git_cleaner.rb
+++ lib/git_cleaner.rb
@@ -1,6 +1,212 @@
 require "git_cleaner/version"
+require "thor"
+require "highline"
+require "rainbow"
+require "httpclient"
+require "git"
 
 module GitCleaner
   class Error < StandardError; end
   # Your code goes here...
+
+  class Cleaner < Thor
+
+    desc 'clean', 'Clean git branches'
+    option :local
+    option :origin
+    def cleanup
+      cleanup_local if options[:local]
+      cleanup_origin if options[:origin]
+      StaticMethods.new.print_no_flag_error_message if options.empty?
+    end
+
+    desc 'preview', 'Preview git branches that had been merged into master'
+    option :local
+    option :origin
+    def preview
+      preview_local if options[:local]
+      preview_origin if options[:origin]
+      StaticMethods.new.print_no_flag_error_message if options.empty?
+    end
+
+    no_commands do
+      def preview_local
+        Git.configure do |config|
+          # If you want to use a custom git binary
+          config.binary_path = "/usr/bin/git"
+        end
+        current_directory = Dir.pwd
+
+        StaticMethods.new.print_preview_message('local')
+        begin
+          g = Git.open("#{current_directory}")
+
+          g.branches.local.each_with_index do |local_branch, index|
+            puts "    🧹 #{index + 1}. #{local_branch}"
+          end
+
+          puts "\n\n"
+        rescue
+          StaticMethods.new.print_no_git_error_message
+        end
+      end
+    end
+
+    no_commands do
+      def preview_origin
+        Git.configure do |config|
+          # If you want to use a custom git binary
+          config.binary_path = "/usr/bin/git"
+        end
+        current_directory = Dir.pwd
+
+        StaticMethods.new.print_preview_message('origin')
+        begin
+          g = Git.open("#{current_directory}")
+
+          g.branches.remote.each_with_index do |origin_branch, index|
+            puts "    🧹 #{index + 1}. #{origin_branch}"
+          end
+
+          puts "\n\n"
+        rescue
+          StaticMethods.new.print_no_git_error_message
+        end
+      end
+    end
+
+    no_commands do
+      def cleanup_local
+        # Git.configure do |config|
+        #   # If you want to use a custom git binary
+        #   config.binary_path = "/usr/bin/git"
+        # end
+        # git_object = Git.init
+        #
+        # git_object.config('user.name', 'KarimEbrahemAbdelaziz')
+        # git_object.config('user.email', 'karimabdelazizmansour@gmail.com')
+        # g.config('user.name', g.config('user.name'))
+        # g.config('user.email', g.config('user.email'))
+
+        current_directory = Dir.pwd
+
+        StaticMethods.new.print_cleanup_message
+        begin
+          g = Git.open("#{current_directory}")
+
+          g.branches.local.each_with_index do |local_branch, index|
+            puts "    🧹 #{index + 1}. #{local_branch}"
+          end
+
+          puts "\n\n"
+
+          inputStream = HighLine.new
+          should_delete_branches = inputStream.agree("Delete these branches? (y/n) ")
+          puts "\n\n"
+          if should_delete_branches == true
+            g.checkout('master')
+            g.branches.local.each_with_index do |local_branch, index|
+              begin
+                if g.branch('master').contains?(local_branch)
+                  local_branch.delete
+                  puts "    #{index + 1}. '#{local_branch}' Removed ✅"
+                else
+                  puts "    #{index + 1}. '#{local_branch}' Not merged into 'master' ❌"
+                end
+              rescue
+                puts "    #{index + 1}. Can't remove '#{local_branch}' ❌"
+              end
+            end
+          end
+          puts "\n\n"
+        rescue
+          StaticMethods.new.print_no_git_error_message
+        end
+      end
+    end
+
+    no_commands do
+      def cleanup_origin
+        Git.configure do |config|
+          # If you want to use a custom git binary
+          config.binary_path = "/usr/bin/git"
+        end
+        current_directory = Dir.pwd
+
+        StaticMethods.new.print_cleanup_message
+        begin
+          g = Git.open("#{current_directory}")
+
+          g.branches.remote.each_with_index do |origin_branch, index|
+            puts "    🧹 #{index + 1}. #{origin_branch}"
+          end
+
+          puts "\n\n"
+        rescue
+          StaticMethods.new.print_no_git_error_message
+        end
+      end
+    end
+
+  end
+
+  class StaticMethods
+    def print_cleanup_message
+      text2 = "
+   _____ _ _      _____ _
+  / ____(_) |    / ____| |
+ | |  __ _| |_  | |    | | ___  __ _ _ __   ___ _ __
+ | | |_ | | __| | |    | |/ _ \\/ _` | '_ \\ / _ \\ '__|
+ | |__| | | |_  | |____| |  __/ (_| | | | |  __/ |
+  \\_____|_|\\__|  \\_____|_|\\___|\\__,_|_| |_|\\___|_|
+"
+      puts "#{text2} \n"
+      puts "Welcome to the #{Rainbow("Git Cleaner").underline.bright.red} 🧹🧹 project!
+Before we start cleaning your git branches, here is a preview of your branches: \n\n"
+    end
+
+    def print_preview_message(git_type)
+      text2 = "
+   _____ _ _      _____ _
+  / ____(_) |    / ____| |
+ | |  __ _| |_  | |    | | ___  __ _ _ __   ___ _ __
+ | | |_ | | __| | |    | |/ _ \\/ _` | '_ \\ / _ \\ '__|
+ | |__| | | |_  | |____| |  __/ (_| | | | |  __/ |
+  \\_____|_|\\__|  \\_____|_|\\___|\\__,_|_| |_|\\___|_|
+"
+      puts "#{text2} \n"
+      puts "Welcome to the #{Rainbow("Git Cleaner").underline.bright.red} 🧹🧹 project!
+Here is a preview of your #{git_type} branches: \n\n"
+    end
+
+    def print_no_git_error_message
+      text2 = "
+   _____ _ _      _____ _
+  / ____(_) |    / ____| |
+ | |  __ _| |_  | |    | | ___  __ _ _ __   ___ _ __
+ | | |_ | | __| | |    | |/ _ \\/ _` | '_ \\ / _ \\ '__|
+ | |__| | | |_  | |____| |  __/ (_| | | | |  __/ |
+  \\_____|_|\\__|  \\_____|_|\\___|\\__,_|_| |_|\\___|_|
+"
+      puts "#{text2} \n"
+      puts "Welcome to the #{Rainbow("Git Cleaner").underline.bright.red} 🧹🧹 project! \n\n"
+      puts "⚠️⚠️ There is no #{Rainbow("git").underline.bright.red} directory here 😡 \n\n"
+    end
+
+    def print_no_flag_error_message
+      text2 = "
+   _____ _ _      _____ _
+  / ____(_) |    / ____| |
+ | |  __ _| |_  | |    | | ___  __ _ _ __   ___ _ __
+ | | |_ | | __| | |    | |/ _ \\/ _` | '_ \\ / _ \\ '__|
+ | |__| | | |_  | |____| |  __/ (_| | | | |  __/ |
+  \\_____|_|\\__|  \\_____|_|\\___|\\__,_|_| |_|\\___|_|
+"
+      puts "#{text2} \n"
+      puts "Welcome to the #{Rainbow("Git Cleaner").underline.bright.red} 🧹🧹 project! \n\n"
+      puts "⚠️⚠️ You didn't specify any flag, please use --local or --origin flag 😡 \n\n"
+    end
+  end
+
+
 end
