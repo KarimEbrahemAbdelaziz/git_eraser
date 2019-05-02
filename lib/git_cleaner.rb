33bc0bebfeb7c7d7c564dfef613054f42614cc9a
KarimEbrahemAbdelaziz <karimabdelazizmansour@gmail.com>
2019-05-02 05:34:33 +0200

    Add delete origin branchs feature

--- lib/git_cleaner.rb
+++ lib/git_cleaner.rb
@@ -7,17 +7,16 @@ require "git"
 
 module GitCleaner
   class Error < StandardError; end
-  # Your code goes here...
 
   class Cleaner < Thor
 
-    desc 'clean', 'Clean git branches'
+    desc 'cleanup', 'Clean git branches that had been merged into master'
     option :local
     option :origin
     def cleanup
       cleanup_local if options[:local]
       cleanup_origin if options[:origin]
-      StaticMethods.new.print_no_flag_error_message if options.empty?
+      Helper.new.print_no_flag_error_message if options.empty?
     end
 
     desc 'preview', 'Preview git branches that had been merged into master'
@@ -26,7 +25,7 @@ module GitCleaner
     def preview
       preview_local if options[:local]
       preview_origin if options[:origin]
-      StaticMethods.new.print_no_flag_error_message if options.empty?
+      Helper.new.print_no_flag_error_message if options.empty?
     end
 
     no_commands do
@@ -37,7 +36,7 @@ module GitCleaner
         end
         current_directory = Dir.pwd
 
-        StaticMethods.new.print_preview_message('local')
+        Helper.new.print_preview_message('local')
         begin
           g = Git.open("#{current_directory}")
 
@@ -47,7 +46,7 @@ module GitCleaner
 
           puts "\n\n"
         rescue
-          StaticMethods.new.print_no_git_error_message
+          Helper.new.print_no_git_error_message
         end
       end
     end
@@ -60,7 +59,7 @@ module GitCleaner
         end
         current_directory = Dir.pwd
 
-        StaticMethods.new.print_preview_message('origin')
+        Helper.new.print_preview_message('origin')
         begin
           g = Git.open("#{current_directory}")
 
@@ -70,27 +69,16 @@ module GitCleaner
 
           puts "\n\n"
         rescue
-          StaticMethods.new.print_no_git_error_message
+          Helper.new.print_no_git_error_message
         end
       end
     end
 
     no_commands do
       def cleanup_local
-        # Git.configure do |config|
-        #   # If you want to use a custom git binary
-        #   config.binary_path = "/usr/bin/git"
-        # end
-        # git_object = Git.init
-        #
-        # git_object.config('user.name', 'KarimEbrahemAbdelaziz')
-        # git_object.config('user.email', 'karimabdelazizmansour@gmail.com')
-        # g.config('user.name', g.config('user.name'))
-        # g.config('user.email', g.config('user.email'))
-
         current_directory = Dir.pwd
 
-        StaticMethods.new.print_cleanup_message
+        Helper.new.print_cleanup_message
         begin
           g = Git.open("#{current_directory}")
 
@@ -102,8 +90,8 @@ module GitCleaner
 
           inputStream = HighLine.new
           should_delete_branches = inputStream.agree("Delete these branches? (y/n) ")
-          puts "\n\n"
           if should_delete_branches == true
+            puts "\n\n"
             g.checkout('master')
             g.branches.local.each_with_index do |local_branch, index|
               begin
@@ -120,20 +108,16 @@ module GitCleaner
           end
           puts "\n\n"
         rescue
-          StaticMethods.new.print_no_git_error_message
+          Helper.new.print_no_git_error_message
         end
       end
     end
 
     no_commands do
       def cleanup_origin
-        Git.configure do |config|
-          # If you want to use a custom git binary
-          config.binary_path = "/usr/bin/git"
-        end
         current_directory = Dir.pwd
 
-        StaticMethods.new.print_cleanup_message
+        Helper.new.print_cleanup_message
         begin
           g = Git.open("#{current_directory}")
 
@@ -142,15 +126,65 @@ module GitCleaner
           end
 
           puts "\n\n"
+
+          inputStream = HighLine.new
+          should_delete_branches = inputStream.agree("Delete these branches? (y/n) ")
+
+          if should_delete_branches == true
+            puts "\n\n"
+            g.checkout('master')
+            g.pull('origin', 'master')
+            array_of_remote_deleted_branches = []
+            g.branches.remote.each_with_index do |origin_branch, index|
+              begin
+                branch_string = origin_branch.to_s
+                branch_string['remotes/origin/'] = ''
+                g.checkout(branch_string)
+                if g.branch('master').contains?(branch_string) && branch_string != 'master'
+                  system "git push -u origin --delete #{branch_string} -q"
+                  puts "    #{index + 1}. '#{origin_branch}' Removed ✅"
+                  array_of_remote_deleted_branches.append(branch_string)
+                elsif branch_string == 'master'
+                  puts "    #{index + 1}. '#{origin_branch}' Can't remove 'master' branch ❌"
+                else
+                  puts "    #{index + 1}. '#{origin_branch}' Not merged into 'master' ❌"
+                end
+              rescue
+                puts "    #{index + 1}. Can't remove '#{origin_branch}' ❌"
+              end
+            end
+            g.checkout('master')
+            delete_local_after_origin(array_of_remote_deleted_branches)
+          end
+          puts "\n\n"
         rescue
-          StaticMethods.new.print_no_git_error_message
+          Helper.new.print_no_git_error_message
+        end
+      end
+    end
+
+    no_commands do
+      def delete_local_after_origin(branches)
+        current_directory = Dir.pwd
+        begin
+          g = Git.open("#{current_directory}")
+          g.branches.local.each do |local_branch|
+            puts "Local: #{local_branch}"
+            branches.each do |value|
+              puts "coming: #{value}"
+              if local_branch.to_s == value
+                local_branch.delete
+              end
+            end
+          end
+        rescue
         end
       end
     end
 
   end
 
-  class StaticMethods
+  class Helper
     def print_cleanup_message
       text2 = "
    _____ _ _      _____ _
